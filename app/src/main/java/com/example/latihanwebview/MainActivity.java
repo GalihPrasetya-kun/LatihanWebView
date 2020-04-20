2ce292226356aec8b6c7a0a24fe38a86073b967f
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2020-04-20 15:03:35 +0700

    Pembuatan Aplikasi

---
+++ app/src/main/java/com/example/latihanwebview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.latihanwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.myWebView);
+
+        WebView myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("http://hirupmotekar.com/");
+
+    }
+}
